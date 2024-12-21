2218ee9ca4194eba1077ab6be522d1f56ddeca21
dac <dac@students.acts.in>
2024-12-21 17:08:43 +0530

    1stcommit

---
+++ Assignment1/HelloDocker.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class HelloDocker{
+	public static void main(String []args){
+		System.out.println("Hello Docker!!!");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your number 1:");
+		int num1 = sc.nextInt();
+		System.out.println("Enter your number 2:");
+		int num2 = sc.nextInt();
+		int sum = num1 + num2;
+		System.out.println("Sum of two number is = "+sum);
+		sc.close();
+		
+	}
+}
